cd6b58137b53814d63e701c55ba5828d754384f6
Bowie <marketing@letscode.com.br>
2024-04-06 22:18:10 -0300

    get attendees badge

--- src/server.ts
+++ src/server.ts
@@ -5,6 +5,8 @@ import {
 } from "fastify-type-provider-zod";
 import { createEvent } from "./routes/create-event";
 import { registerForEvent } from "./routes/register-for-event";
+import { getEvent } from "./routes/get-event";
+import { getAttendeesBadge } from "./routes/get-attendees-badge";
 
 const app = fastify();
 
@@ -13,6 +15,8 @@ app.setSerializerCompiler(serializerCompiler);
 
 app.register(createEvent);
 app.register(registerForEvent);
+app.register(getEvent);
+app.register(getAttendeesBadge);
 
 app.listen({ port: 3333 }).then(() => {
   console.log("http server running");
